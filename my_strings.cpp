e93e6f667ed4410c9672cb45cf1f98f5055d3b5f
Kaly-0 <flavia.fernandes@oteria.fr>
2026-03-17 13:52:25 -0400

    modification

--- my_strings.cpp
+++ my_strings.cpp
@@ -4,5 +4,13 @@
 int main() {
 	std::string filename = "test";
 	std::ifstream file(filename, std::ios::binary);
-		if (!file);
+		if (!file)
 			return 1;
+
+		char c;
+
+		while (file.get(c)) {
+			std::cout << c;
+
+		};
+};
