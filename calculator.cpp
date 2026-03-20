295325f516a892cb5987a87c6e9e4d48c60370ca
Kaly-0 <flavia.fernandes@oteria.fr>
2026-03-20 09:48:11 -0400

    Première version de calculator qui demande et renvoie 2 chiffres

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () {
+	int number1;
+	int number2;
+	std::string operateur;
+
+	std::cout << "Enter the first number :";
+        std::cin >> number1;
+	std::cout << "Enter the second number :";	
+	std::cin >> number2;
+}
+
