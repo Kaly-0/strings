4becc20cd8f64108248a04fa83d66e803c381162
Kaly-0 <flavia.fernandes@oteria.fr>
2026-03-21 10:01:02 -0400

    Calculatrice fonctionnelle

--- calculator.cpp
+++ calculator.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+using namespace std;
 
 int main () {
-	int number1;
-	int number2;
-	std::string operateur;
+	double number1;
+	double number2;
+	char operateur;
+	double resultat;
 
-	std::cout << "Enter the first number :";
-        std::cin >> number1;
-	std::cout << "Enter the second number :";	
-	std::cin >> number2;
+
+	cout << "Enter the first number : ";
+        cin >> number1;
+	cout << "Enter the operator (+,-,*,/) : ";
+	cin >> operateur;
+	cout << "Enter the second number: ";
+	cin >> number2;
+		switch (operateur) {
+			case '+':
+				resultat = number1 + number2;
+				break;
+			case '-':
+				resultat = number1 - number2;
+				break;
+			case '*':
+				resultat = number1 * number2;
+				break;
+			case '/':
+				if (number1 == 0) {
+					cout << "Erreur: division par 0 impossible \n"; }
+				else {					
+					resultat = number1 / number2;
+				}
+				break;
+}
+
+	cout << "Résultat " << number1 << " " << operateur << " " << number2 << " = " << resultat << "  \n";
+	return 0;
 }
 
